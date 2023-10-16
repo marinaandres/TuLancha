2209182985228795c92a278c72b79c18a428d3d9
Marina Andrés Aragón <128967795+marinaandres@users.noreply.github.com>
2023-10-16 09:02:27 +0200

    TabViewCustom

---
+++ Tu Lancha/TabViewCustom.swift
@@ -0,0 +1,46 @@
+//
+//  TabViewCustom.swift
+//  Tu Lancha
+//
+//  Created by Marina Andrés Aragón on 16/10/23.
+//
+
+import SwiftUI
+
+struct TabViewCustom: View {
+    var body: some View {
+        TabView {
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Buscar")
+                }
+                .padding(.top, 30)
+            
+            MessageView()
+                .tabItem {
+                    Image(systemName: "ellipsis.message")
+                    Text("Chat")
+                }
+                .edgesIgnoringSafeArea(.top)
+            CalendarView()
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("Reservas")
+                }
+                .edgesIgnoringSafeArea(.top)
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+                    Text("Perfil")
+                }
+               
+        }
+}
+    
+    struct TabView_Previews: PreviewProvider {
+        static var previews: some View {
+            TabViewCustom()
+        }
+    }
+}
