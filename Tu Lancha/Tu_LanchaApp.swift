1d978d49678be5949fe353a2060d80611ddd8e53
Marina Andrés Aragón <128967795+marinaandres@users.noreply.github.com>
2023-10-16 08:56:31 +0200

    Initial Commit

---
+++ Tu Lancha/Tu_LanchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tu_LanchaApp.swift
+//  Tu Lancha
+//
+//  Created by Marina Andrés Aragón on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Tu_LanchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
