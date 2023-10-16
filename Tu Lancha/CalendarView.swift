efdaaf11ca182899f7b5268d415aabbb06da0bef
Marina Andrés Aragón <128967795+marinaandres@users.noreply.github.com>
2023-10-16 09:11:12 +0200

    CalendarView

---
+++ Tu Lancha/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  Tu Lancha
+//
+//  Created by Marina Andrés Aragón on 16/10/23.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
